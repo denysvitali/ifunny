67aff7ad7fb2ff3497a0382593f23c1c0bf7d727
Denys Vitali <denys@denv.it>
2018-10-19 13:56:49 +0200

    Change namespace

--- src/ch/supsi/dti/i3b/husky/ifunny/Main.java
+++ src/ch/supsi/dti/i3b/husky/ifunny/Main.java
@@ -1,8 +1,8 @@
-package com.company;
+package ch.supsi.dti.i3b.husky.ifunny;
 
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    // write your code here
     }
 }
