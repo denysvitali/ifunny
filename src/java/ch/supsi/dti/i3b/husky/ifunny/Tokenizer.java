890a3aa7f4adbc05eedcd9062bf44529c53379b0
Filippo <purafilippo@gmail.com>
2018-10-19 16:06:46 +0200

    implemented peek check

--- src/java/ch/supsi/dti/i3b/husky/ifunny/Tokenizer.java
+++ src/java/ch/supsi/dti/i3b/husky/ifunny/Tokenizer.java
@@ -26,7 +26,7 @@ public class Tokenizer {
         return c >= '0' && c <= '9';
     }
 
-    public Token nextToken() throws IOException {
+    public void nextToken() throws IOException {
 
         currentChar = r.read();
         while(Character.isWhitespace(currentChar)){
@@ -53,37 +53,115 @@ public class Tokenizer {
                 token = new Token(Token.Type.CLSRNBRACKETS);
                 break;
             case '<':
-                //TODO: implement peek check
+                if(peekChar() == '=') {
+                    currentChar = r.read();
+                    token = new Token(Token.Type.MINEQ);
+                }
+                else{
+                    token = new Token(Token.Type.MIN);
+                }
                 break;
             case '>':
-                //TODO: implement peek check
+                if(peekChar() == '=') {
+                    currentChar = r.read();
+                    token = new Token(Token.Type.MAJEQ);
+                }
+                else{
+                    token = new Token(Token.Type.MAJ);
+                }
                 break;
             case '*':
-                //TODO: implement peek check
+                if(peekChar() == '=') {
+                    currentChar = r.read();
+                    token = new Token(Token.Type.ASSIGNMULT);
+                }
+                else{
+                    token = new Token(Token.Type.MULT);
+                }
                 break;
             case '/':
-                //TODO: implement peek check
+                if(peekChar() == '=') {
+                    currentChar = r.read();
+                    token = new Token(Token.Type.ASSIGNMDIV);
+                }
+                else if(peekChar() == '/'){
+                    currentChar = r.read();
+                    //TODO: create commentLine function (this should recall next after is done)
+                    commentLine();
+                }
+                else if(peekChar() == '*'){
+                    currentChar = r.read();
+                    //TODO: create commentBlock function (this should recall next after is done)
+                    commentBlock();
+                }
+                else{
+                    token = new Token(Token.Type.DIV);
+                }
                 break;
             case '%':
-                //TODO: implement peek check
+                if(peekChar() == '=') {
+                    currentChar = r.read();
+                    token = new Token(Token.Type.ASSIGNMOD);
+                }
+                else{
+                    token = new Token(Token.Type.MOD);
+                }
                 break;
             case '+':
-                //TODO: implement peek check
+                if(peekChar() == '=') {
+                    currentChar = r.read();
+                    token = new Token(Token.Type.ASSIGNMSUM);
+                }
+                else{
+                    token = new Token(Token.Type.SUM);
+                }
                 break;
             case '-':
-                //TODO: implement peek check
+                if(peekChar() == '=') {
+                    currentChar = r.read();
+                    token = new Token(Token.Type.ASSIGNMSUB);
+                }
+                else{
+                    token = new Token(Token.Type.SUB);
+                }
                 break;
             case '=':
-                //TODO: implement peek check
+                if(peekChar() == '=') {
+                    currentChar = r.read();
+                    token = new Token(Token.Type.EQ);
+                }
+                else{
+                    token = new Token(Token.Type.ASSIGNM);
+                }
                 break;
             case '!':
-                //TODO: implement peek check
+                if(peekChar() == '=') {
+                    currentChar = r.read();
+                    token = new Token(Token.Type.NOTEQ);
+                }
+                else{
+                    token = new Token(Token.Type.NOT);
+                }
                 break;
             case '&':
-                //TODO: implement peek check
+                if(peekChar() == '&') {
+                    currentChar = r.read();
+                    token = new Token(Token.Type.AND);
+                }
+                else{
+                    //TODO: not a valid token exception
+                    token = new Token(Token.Type.UNKNOW);
+                }
                 break;
             case '|':
-                //TODO: implement peek check
+                if(peekChar() == '|') {
+                    currentChar = r.read();
+                    token = new Token(Token.Type.OR);
+                }
+                else{
+                    //TODO: not a valid token exception
+                    token = new Token(Token.Type.UNKNOW);
+                }
                 break;
             case '"':
                 readStr();
@@ -97,6 +175,10 @@ public class Tokenizer {
                 idOrKeyword();
                 break;
         }
+    }
+
+    public Token getNextToken() throws IOException {
+        nextToken();
         return token;
     }
 
