542abb5d4e4f31241b2392acea4c02dfaa6e17ff
Denys Vitali <denys@denv.it>
2018-11-09 14:01:34 +0100

    Implement Expr, Env, Val

---
+++ src/main/java/ch/supsi/dti/i3b/husky/ifunny/expressions/Expr.java
@@ -0,0 +1,8 @@
+package ch.supsi.dti.i3b.husky.ifunny.expressions;
+
+import ch.supsi.dti.i3b.husky.ifunny.Env;
+import ch.supsi.dti.i3b.husky.ifunny.Val;
+
+public abstract class Expr {
+	abstract Val eval(Env env);
+}
---
+++ src/main/java/ch/supsi/dti/i3b/husky/ifunny/expressions/WhileExpr.java
@@ -0,0 +1,22 @@
+package ch.supsi.dti.i3b.husky.ifunny.expressions;
+
+import ch.supsi.dti.i3b.husky.ifunny.Env;
+import ch.supsi.dti.i3b.husky.ifunny.Val;
+
+public class WhileExpr extends Expr {
+	private Expr condition;
+	private Expr body;
+
+	public WhileExpr(Expr condition, Expr body){
+		this.condition = condition;
+		this.body = body;
+	}
+
+	@Override
+	Val eval(Env env) {
+		while(this.condition.eval(env).getValue().equals(true)){
+			this.body.eval(env);
+		}
+		return null;
+	}
+}
