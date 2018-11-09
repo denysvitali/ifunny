94e21368fa82f418939498d78e5ff91d8522cf2f
Denys Vitali <denys@denv.it>
2018-11-09 14:30:06 +0100

    Add IfExpr

---
+++ src/main/java/ch/supsi/dti/i3b/husky/ifunny/expressions/IfExpr.java
@@ -0,0 +1,25 @@
+package ch.supsi.dti.i3b.husky.ifunny.expressions;
+
+import ch.supsi.dti.i3b.husky.ifunny.Env;
+import ch.supsi.dti.i3b.husky.ifunny.Val;
+
+public class IfExpr extends Expr {
+
+	private Expr ifEvaluation;
+	private Expr ifBody;
+	private Expr elseBody;
+
+	public IfExpr(Expr eval, Expr body, Expr elseBody){
+		this.ifEvaluation = eval;
+		this.ifBody = body;
+		this.elseBody = elseBody;
+	}
+
+	@Override
+	Val eval(Env env) {
+		if(this.ifEvaluation.eval(env).getValue().equals(true)){
+			return this.ifBody.eval(env);
+		}
+		return this.elseBody.eval(env);
+	}
+}
